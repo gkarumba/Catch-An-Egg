6c46de6c25d575ad1eb475bc66a0e158638b3bbc
gkarumba <gachegua@gmail.com>
2019-03-10 23:33:45 +0300

    Make the UI page

---
+++ function.js
@@ -0,0 +1,6 @@
+// var egg_initial_position = parseInt(egg.style('top'));
+// var egg = document.getElementsByClassName('egg');
+// function egg_down(egg){
+//     egg.style('top', egg_initial_position  + 2);
+// }
+// // module.exports = egg_down;
---
+++ script.js
@@ -0,0 +1,9 @@
+// // const egg_down = require('egg_down');
+// var egg1 = document.getElementById('egg1');
+// var the_game = () => {
+//     // console.log(123)
+//     egg_down(egg1);
+//     var anim_id = requestAnimationFrame(the_game);
+//     // cancelAnimationFrame(anim_id);
+// }
+// var anim_id = requestAnimationFrame(the_game);
